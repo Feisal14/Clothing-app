3700ff954130bdf29798ed04417e1df78e09d122
Your NameFeisal14 <fisalgemar@gmail.com>
2023-12-23 21:34:25 +0300

    Added Routes

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import Logo from "../../assets/crown.svg";
+import "./navigations.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={Logo} alt="logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
